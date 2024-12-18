b624dccfb1ada1fec23750b54a0babeefee193af
Ranjun <591962415@qq.com>
2024-12-18 18:53:57 +0800

    init

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline  shadow-2xl font-sans text-red-300">
+      Hello world!
+    </h1>
+    <van-icon name="chat-o" />
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
